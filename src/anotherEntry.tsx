d80f538b6e13c5e7d423dcb128f16908f70b8ea1
Wan Muzaffar <wanmuz86@gmail.com>
2025-12-05 12:01:07 +0800

    anotherEntry.tsx

---
+++ src/anotherEntry.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client';
+
+const AnotherComponent:React.FC = () => {
+  return (
+    <div>
+        <h1>Another Component</h1>
+        <p>This is another component</p>
+    </div>
+  )
+}
+
+const container = document.getElementById("root");;
+if (container) {
+    const root = createRoot(container);
+    root.render(<AnotherComponent/>);
+}
